c01b1a994db889a125c62d8f5029d71ec6135614
yuvasree <kandruyuvasree@gmail.com>
2022-10-23 00:00:34 +0300

    login done

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,17 +3,18 @@
 <div class="container pt-5 pb-5">
     <div class="row justify-content-md-center">
         <div class="col-sm-4 ">
-        <form>
+  <form action="/login" method="POST">
+    @csrf
   <div class="form-group">
     <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
   </div>
   <div class="form-group">
     <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
+    <input type="password" name="password" class="form-control" id="exampleInputPassword1">
   </div>
   
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" class="btn btn-primary">Login</button>
 </form>
 
         </div>
